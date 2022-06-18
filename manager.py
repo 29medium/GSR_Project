e8fb96cfbbfc64b6e5d52b5de7f1753a628c1d70
Paulo Sousa <45037062+29medium@users.noreply.github.com>
2022-06-18 01:38:42 +0100

    pedidos entre manager e proxy a funcionar

--- manager.py
+++ manager.py
@@ -1,32 +1,107 @@
-from Crypto.Cipher import AES
+import threading
 from socket import *
+import time
 
 
-def do_encrypt(message):
-    obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-    ciphertext = obj.encrypt(message)
-    return ciphertext
+requests = set()
+request_number = 0
+lock = threading.Lock()
 
-def do_decrypt(ciphertext):
-    obj2 = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-    message = obj2.decrypt(ciphertext)
-    return message
+def get_request_number():
+    global request_number
+    
+    request_number += 1
+    return str(request_number)
+
+def ask_responses(s):
+    global requests, lock
+
+    while True:
+        time.sleep(5)
+
+        lock.acquire()
+        for r in requests:
+            data = bytes("get_response," + r, encoding='utf8')
+            s.sendall(data)
+            time.sleep(0.5)
+        lock.release()
+
+def receive_responses(s):
+    global requests
+
+    while True:
+        data = s.recv(1024).decode('utf8')
+
+        print(data)
+
+        l = data.split(",")
+
+        snmp_type = l[0]
+        request_number = l[1]
+
+        if snmp_type=="response":
+            response_type = l[2]
+
+            if response_type=="true":
+                lock.acquire()
+                requests.remove(request_number)
+                lock.release()
+
+                for elem in l[3:]:
+                    print(elem)
+
+            elif response_type=="error":
+                lock.acquire()
+                requests.remove(request_number)
+                lock.release()
+
+                print(l[3])
+
+        elif snmp_type=="ack":
+            requests.add(request_number)
+
+        else:
+            s.close()
 
 def main():
+    global request_number
+
     HOST = "localhost"
     PORT = 65432
 
     s = socket()
     s.connect((HOST,PORT))
 
-    #data = do_encrypt("192.168.1.68,public,1.3.6.1.2.1.1.1.0")
-    #s.sendAll(data)
+    threading.Thread(target=receive_responses, args=[s]).start()
+    threading.Thread(target=ask_responses, args=[s]).start()
 
-    data = bytes("192.168.1.68,public,1.3.6.1.2.1.1.1.0", encoding='utf8')
+    data = bytes("get," + get_request_number() + ",192.168.1.68,public,1.3.6.1.2.1.1.1.0", encoding='utf8')
     s.sendall(data)
 
-    # print(do_decrypt(s.recv()))
-    data = s.recv(1024).decode('utf8')
-    print(data)
+    time.sleep(0.5)
+
+    data = bytes("get_next," + get_request_number() + ",192.168.1.68,public,1.3.6.1.2.1.1.1.0", encoding='utf8')
+    s.sendall(data)
+
+    time.sleep(0.5)
+
+    data = bytes("get_bulk," + get_request_number() + ",192.168.1.68,public,0,10,1.3.6.1.2.1.1.3.0", encoding='utf8')
+    s.sendall(data)
+
+    while True:
+        type = input("Insert type")
+        ip = input("Insert host")
+        community = input("Insert community string")
+        oids = input("Insert oids seperated by comma")
+
+        if(type=="get_bulk"):
+            nonRepeaters = input("Insert non repeaters")
+            maxRepetitions = input("Insert max repetitions")
+            
+            data = bytes(type + "," + get_request_number() + ',' + ip + "," + community + "," + nonRepeaters + "," + maxRepetitions + ',' + oids, encoding='utf8')
+            s.sendall(data) 
+        else:
+            data = bytes(type + "," + get_request_number() + ',' + ip + "," + community + "," + oids, encoding='utf8')
+            s.sendall(data) 
 
 main()
--- proxy.py
+++ proxy.py
@@ -3,55 +3,102 @@ from threading import Thread
 from pysnmp.hlapi import *
 from Crypto.Cipher import AES
 
-def do_encrypt(message):
-    obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-    ciphertext = obj.encrypt(message)
-    return ciphertext
 
-def do_decrypt(ciphertext):
-    obj2 = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-    message = obj2.decrypt(ciphertext)
+results = dict()
 
-    return str(message)
-
-def fetch(handler):    
+def fetch(addr, request_number, handler):  
     error_indication, error_status, error_index, var_binds = next(handler)
 
     if not error_indication and not error_status:
-        items = list()
+        if addr not in results:
+            results[addr] = dict()
 
+        results[addr][request_number] = list()
         for var_bind in var_binds:
-            items.append(var_bind[1])
+            results[addr][request_number].append(var_bind[1])
 
     else:
-        raise RuntimeError('Got SNMP error: {0}'.format(error_indication))
-
-    return str(items[0])
+        results[addr][request_number] = 'Got SNMP error: {0}'.format(error_indication)
 
 def clientHandler(conn, addr):
+    global results
+
     print ("Manager " + addr[0] + " " + str(addr[1]) + " is connected")
 
     while True:
-        #data = do_decrypt(conn.recv(1024))
         data = conn.recv(1024).decode('utf8')
+
+        print(data)
 
         l = data.split(",")
 
-        host = l[0]
-        com_str = l[1]
-        mib = l[2]
+        snmp_type = l[0]
+        request_number = l[1]
 
-        handler = getCmd(
-            SnmpEngine(),
-            CommunityData(com_str, mpModel=0),
-            UdpTransportTarget((host, 161)),
-            ContextData(),
-            ObjectType(ObjectIdentity(mib))
-        )
+        if snmp_type=="get":
+            host = l[2]
+            com_str = l[3]
+            mib = list(map(lambda s : ObjectType(ObjectIdentity(s)), l[4:]))
 
-        #data = do_encrypt(fetch(handler))
-        data = bytes(fetch(handler), encoding='utf8')
-        conn.sendall(data)
+            data = bytes("ack," + request_number, encoding='utf8')
+            conn.sendall(data)
+
+            fetch(addr, request_number, getCmd(
+                SnmpEngine(),
+                CommunityData(com_str, mpModel=1),
+                UdpTransportTarget((host, 161)),
+                ContextData(),
+                *mib
+            ))
+
+        elif snmp_type=="get_next":
+            host = l[2]
+            com_str = l[3]  
+            mib = list(map(lambda s : ObjectType(ObjectIdentity(s)), l[4:]))
+
+            data = bytes("ack," + request_number, encoding='utf8')
+            conn.sendall(data)
+
+            fetch(addr, request_number, nextCmd(
+                SnmpEngine(),
+                CommunityData(com_str, mpModel=1),
+                UdpTransportTarget((host, 161)),
+                ContextData(),
+                *mib
+            ))
+
+        elif snmp_type=="get_bulk":
+            host = l[2]
+            com_str = l[3]
+            nonRepeaters = int(l[4])
+            maxRepetitions = int(l[5])
+            mib = list(map(lambda s : ObjectType(ObjectIdentity(s)), l[6:]))
+
+            data = bytes("ack," + request_number, encoding='utf8')
+            conn.sendall(data)
+
+            fetch(addr, request_number, bulkCmd(
+                SnmpEngine(),
+                CommunityData(com_str, mpModel=1),
+                UdpTransportTarget((host, 161)),
+                ContextData(),
+                nonRepeaters, maxRepetitions,
+                *mib
+            ))
+        
+        elif snmp_type=="get_response":
+            if addr not in results or request_number not in results[addr]:
+                data = bytes("response," + request_number + ",false", encoding='utf8')
+                conn.sendall(data)
+            elif type(results[addr][request_number] is list):
+                for r in results[addr][request_number]:
+                    data = bytes("response," + request_number + ",true," + str(r), encoding='utf8')
+                    conn.sendall(data)
+            else:
+                data = bytes("response," + request_number + ",error," + str(r), encoding='utf8')
+                conn.sendall(data)
+        else:
+            conn.close()
 
 def main():
     HOST = "localhost"
@@ -63,7 +110,7 @@ def main():
 
     while True:
         conn, addr = s.accept()
-
+        
         Thread(target=clientHandler, args=[conn,addr]).start()
 
 main()
